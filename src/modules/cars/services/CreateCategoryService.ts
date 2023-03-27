53bf67d6a05575ce41d1493b1fee6439fb8d15f6
Natally <natallyriqueto@hotmail.com>
2023-03-27 16:35:30 -0400

    add depedency inversion and liskov substitution

---
+++ src/modules/cars/services/CreateCategoryService.ts
@@ -0,0 +1,26 @@
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+class CreateCategoryService {
+    private categoryRepository: ICategoriesRepository;
+
+    constructor(categoryRepository: ICategoriesRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    execute({ name, description }: IRequest): void {
+        const categoryAlreadyExists = this.categoryRepository.findByName(name);
+
+        if(categoryAlreadyExists) {
+            throw new Error('Category already exists!');
+        }
+    
+        this.categoryRepository.create({ name, description });
+    }
+}
+
+export { CreateCategoryService }
