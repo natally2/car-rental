719172373f5366889f52a112e16a004603fd42b5
Natally <natallyriqueto@hotmail.com>
2023-04-17 17:50:19 -0400

    creating user entity

---
+++ src/database/migrations/1681765146415-CreateUsers.ts
@@ -0,0 +1,58 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class CreateUsers1681765146415 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        const tableExists = await queryRunner.hasTable('specifications');
+
+        if (!tableExists) {
+            await queryRunner.createTable(
+                new Table({
+                    name: "users",
+                    columns: [
+                        {
+                            name: "id",
+                            type: "uuid"
+                        },
+                        {
+                            name: "name",
+                            type: "varchar"
+                        },
+                        {
+                            name: "username",
+                            type: "varchar",
+                            isUnique: true
+                        },
+                        {
+                            name: "password",
+                            type: "varchar"
+                        },
+                        {
+                            name: "email",
+                            type: "varchar"
+                        },
+                        {
+                            name: "driver_license",
+                            type: "varchar"
+                        },
+                        {
+                            name: "isAdmin",
+                            type: "boolean",
+                            default: false
+                        },
+                        {
+                            name: "created_at",
+                            type: "timestamp",
+                            default: "now()"
+                        }
+                    ]
+                })
+            )
+        }
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await  queryRunner.dropTable("users");
+    }
+
+}
